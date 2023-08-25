080c68085f6b3d70e4ebaf426e7937a4d8d05a00
BDS <113209967+BDS-001@users.noreply.github.com>
2023-08-25 13:48:10 -0400

    fixed some of the formatting with the buttons on the calculator, aligned buttons for the numbers 1-9

--- calculator.js
+++ calculator.js
@@ -1,10 +1,18 @@
 const buttons = document.querySelector('.buttons')
 
 function createButtons() {
-    for (i=0; i < 10; i++) {
+    let buttonRow = document.createElement('div');
+    buttonRow.setAttribute('class', 'buttonRow')
+    for (i=1; i < 10; i++) {
         const button = document.createElement('button');
         button.innerHTML = i;
-        buttons.appendChild(button)
+        buttonRow.appendChild(button)
+        console.log(buttonRow.innerHTML)
+        if (i % 3 === 0) {
+            buttons.prepend(buttonRow)
+            buttonRow = document.createElement('div')
+            buttonRow.setAttribute('class', 'buttonRow')
+        }
     }
 }
 
